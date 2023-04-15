cd85104f69755689e87523ad6d1532d60cf9c3cd
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-04-15 16:07:57 +0530

    logger added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import logger from "./services/logger.js";
+
 export function add(a:number, b:number): number {
   return a + b;
 }
 
-console.log(add(1,1100));
+logger.info("Result: %d", add(1,1100));
